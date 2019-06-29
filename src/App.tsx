9fd6efb235f60461dfac9e4252f6992009eabc69
Nathanael Couret <nathanael.couret@gmail.com>
2019-06-29 15:56:36 +0200

    Quality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Profile from "./Profile";
 import Search from "./Search";
 import Season from "./Season";
 
-// 501235211
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -17,18 +16,20 @@ const App: React.FC = () => {
           </Link>
         </header>
         <main>
-          <Route exact path="/" component={Search} />
-          <Route
-            exact
-            path="/profile/:id"
-            render={({ match }) => <Profile id={match.params.id} />}
-          />
-          <Route
-            path="/profile/:id/season/:season"
-            render={({ match }) => (
-              <Season id={match.params.id} seasonId={match.params.season} />
-            )}
-          />
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route
+              exact
+              path="/profile/:id"
+              render={({ match }) => <Profile id={match.params.id} />}
+            />
+            <Route
+              path="/profile/:id/season/:season"
+              render={({ match }) => (
+                <Season id={match.params.id} seasonId={match.params.season} />
+              )}
+            />
+          </Switch>
         </main>
       </BrowserRouter>
     </div>
