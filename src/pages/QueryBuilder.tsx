d51d275cfbfcc1cf82796d84c073e69d6d25dfb2
Nathanael Couret <nathanael.couret@gmail.com>
2019-07-20 10:36:24 +0200

    QueryBuilder page as a proper module

---
+++ src/pages/QueryBuilder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const QueryBuilder: React.FC = props => {
+    return (<div>
+        <h4>Query Builder</h4>
+        <form onSubmit={e => e.preventDefault()}>
+
+        </form>
+    </div>)
+}
+
+export default QueryBuilder;
