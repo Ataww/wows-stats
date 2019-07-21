738578014a7664b3a0e1fcb9700b16a03639c202
Nathanael Couret <nathanael.couret@gmail.com>
2019-07-21 21:49:48 +0200

    Query builder first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,42 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import "./App.css";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
 import Season from "./pages/Season";
+import QueryBuilder from "./pages/QueryBuilder";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <header className="App-header">
-          <p>Ranked stuffy stuff</p>
-          <Link to="/" className="App-link">
-            Home
-          </Link>
-        </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Search} />
-            <Route
-              exact
-              path="/profile/:id"
-              render={({ match }) => <Profile id={match.params.id} />}
-            />
-            <Route
-              path="/profile/:id/season/:season"
-              render={({ match }) => (
-                <Season id={match.params.id} seasonId={match.params.season} />
-              )}
-            />
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <header className="App-header">
+                    <p>Ranked stuffy stuff</p>
+                    <Link to="/" className="App-link">
+                        Home
+                    </Link>
+                    <Link to={"/query"} className={"App-link"}>Query</Link>
+                </header>
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={Search}/>
+                        <Route
+                            exact
+                            path="/profile/:id"
+                            render={({match}) => <Profile id={match.params.id}/>}
+                        />
+                        <Route
+                            path="/profile/:id/season/:season"
+                            render={({match}) => (
+                                <Season id={match.params.id} seasonId={match.params.season}/>
+                            )}
+                        />
+                        <Route path={"/query"} render={(_) => <QueryBuilder/>}/>
+                    </Switch>
+                </main>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
--- src/pages/QueryBuilder.tsx
+++ src/pages/QueryBuilder.tsx
@@ -1,11 +1,90 @@
-import React from "react";
+import React, {useEffect, useMemo, useState} from "react";
+import {Field, FieldProps, Form, Formik, FormikProps} from "formik";
+import {
+    accountsStructure,
+    ApiMetadata,
+    apiStructure,
+    Categories,
+    CategoryMetadata,
+    Methods,
+    PlayerMethods
+} from "../common";
+import "./QueryBuilder.scss";
+
+interface QueryParameters {
+    category: keyof ApiMetadata;
+    method: Methods;
+    query: string;
+}
+
+const QueryBuilderForm = ({handleChange, values, touched, errors, setFieldValue, ...props}: FormikProps<QueryParameters>) => {
+    const [category, setCategory] = useState<CategoryMetadata>(accountsStructure);
+    const [methods, setMethods] = useState<React.ReactNode[]>([]);
+
+    const categories = useMemo(() => Object.keys(apiStructure)
+        .map(key => <option key={key}
+                            value={key}>{apiStructure[key].name}</option>
+        ), []);
+
+    useEffect(() => {
+        setCategory(apiStructure[values.category])
+    }, [values.category]);
+
+    useEffect(() => {
+        const nodes: React.ReactNode[] = [];
+        if (category) {
+            for (const path in category.methods) {
+                nodes.push(<option key={path} value={path}>{category.methods[path].method}</option>)
+            }
+            setMethods(nodes);
+        }
+    }, [category]);
+
+    useEffect(() => {
+        setFieldValue("query", `https://api.worldofwarships.eu/wows/${values.category}/${values.method}/`)
+    }, [values.category, values.method, setFieldValue]);
+
+    return (
+        <Form>
+
+            <Field name={"category"} render={({field}: FieldProps<QueryParameters>) => (
+                <div>
+                    <label htmlFor="category-select">Category</label>
+                    <select id="category-select" {...field}>
+                        {categories}
+                    </select>
+                    {touched.category && errors.category && errors.category}
+                </div>)}/>
+            <Field name={"method"} render={({field}: FieldProps<QueryParameters>) => (
+                <div>
+                    <label htmlFor="method-select">Method</label>
+                    <select id="method-select" {...field}>
+                        {methods}
+                    </select>
+                </div>
+            )}/>
+            <Field name={"query"} render={({field, form}: FieldProps<QueryParameters>) => <div>
+                <label htmlFor="query-field">Query preview</label>
+                <textarea id="query-field" cols={50} {...field} placeholder="query" onChange={() => {
+                }}>
+                </textarea>
+                {form.touched.query && form.errors.query && form.errors.query}
+            </div>}/>
+        </Form>
+    )
+}
 
 const QueryBuilder: React.FC = props => {
     return (<div>
         <h4>Query Builder</h4>
-        <form onSubmit={e => e.preventDefault()}>
-
-        </form>
+        <Formik initialValues={{query: "", category: Categories.ACCOUNT, method: PlayerMethods.PLAYER_SEARCH}}
+                onSubmit={(values, actions) => {
+                    console.log(JSON.stringify(values));
+                    actions.setSubmitting(false);
+                }}
+        >
+            {(props: FormikProps<QueryParameters>) => <QueryBuilderForm {...props}  />}
+        </Formik>
     </div>)
 }
 
